a8498b7ac6e4b98556998fcd7460ec681ebab61d
AI-Jiny <key140@naver.com>
2021-09-08 14:09:26 +0900

    Update server.js

--- servers/server.js
+++ servers/server.js
@@ -33,3 +33,56 @@ app.post('/api/python', (req, res)=> {
 
     
 
+    if(username == ""){
+        res.json({'result' : 'NameError'}).end();
+    }
+
+    if(auth == "gov"){   
+        if(username == "" || document1.length != 4 || document1.length != 4 || document1.length != 4 || document1.length != 4){
+            res.json({'result' : 'BlankError'}).end();
+        }
+    }else if(auth == "kdca"){
+        if(username == "" || document1.length != 6 || document2.length != 6 || document3.length != 5){
+            res.json({'result' : 'BlankError'}).end();
+        }
+    }
+
+    let options = {
+        mode: 'text',
+        pythonPath: 'C:\\Users\\Administrator\\anaconda3\\envs\\cert\\python.exe', // Python의 경로를 나타낸다
+        pythonOptions: ['-u'], // get print results in real-time
+        scriptPath: './servers/python/',
+        args:[username,document1,document2,document3,document4]
+        };
+
+    var checkVaccine = new PythonShell('main.py',options);
+
+    checkVaccine.on('message',(result) => {
+        console.log(result)
+        if(result == "True"){
+            res.json({'result' : 'Complete'}).end();
+        }else if(result =="False"){
+            res.json({'result' : 'Error'}).end();
+        }else if(result =="NotFoundDocument"){
+            res.json({'result' : 'NotFoundDocument'}).end();
+        }
+    });
+
+    let pythonKiller = setTimeout(() => {
+        checkVaccine.childProcess.kill();
+        res.json({'result' : 'TimeOutError'}).end();
+    }, time);
+    
+    checkVaccine.end((err,code,signal) => {
+        console.log(err)
+        console.log(code)
+        console.log(signal)
+        clearTimeout(pythonKiller);
+    }); 
+
+
+});
+
+app.listen(port, ()=>{
+    console.log(`express is running on ${port}`);
+})
